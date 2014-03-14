99cf621e5339f608d532649c1537c17dae324837
Will Stevens <wstevens@cloudops.com>
2014-03-14 15:59:12 -0400

    adding templates and volumes to CS

--- migrate_hyperv.py
+++ migrate_hyperv.py
@@ -3,7 +3,10 @@
 from ConfigParser import ConfigParser
 from lib.hyperv import HyperV # the class
 from lib.hyperv import hyperv # the connection
+from lib.cloudstack import cs
 import json
+import hashlib
+import hmac
 import ntpath
 import os
 import pprint
@@ -88,7 +91,7 @@ if __name__ == "__main__":
 							for disk in disks:
 								if 'DriveName' in disk and disk['DriveName'] == 'Hard Drive' and 'DiskImage' in disk:
 									vm_out['disks'].append({
-										'name':ntpath.split(disk['DiskImage'])[1],
+										'name':ntpath.split(disk['DiskImage'])[1].split('.')[0],
 										'url':'%s://%s:%s%s%s' % (
 											'https' if conf.get('WEBSERVER', 'port') == '443' else 'http',
 											conf.get('WEBSERVER', 'host'),
@@ -98,11 +101,11 @@ if __name__ == "__main__":
 											)
 										})
 									print('Copying drive %s' % (disk['DiskImage']))
-									result, ok = copy_vhd_to_webserver(disk['DiskImage'])
-									if ok:
-										print('Finished copy...')
-									else:
-										print('Copy failed...')
+									#result, ok = copy_vhd_to_webserver(disk['DiskImage'])
+									#if ok:
+									#	print('Finished copy...')
+									#else:
+									#	print('Copy failed...')
 
 					# bring the machines back up that were running now that we copied their disks
 					if vm_out['state'] == 'running':
@@ -110,12 +113,88 @@ if __name__ == "__main__":
 						if ok:
 							print('Started the server since it was running at the beginning of this process.')
 
-					print('\nFinished preparing %s' % (vm_in['hyperv_vm_name']))
+					print('Finished preparing %s' % (vm_in['hyperv_vm_name']))
 
 					vms.append(vm_out)
 
 	print "\nBuilt the following details"
 	pprint.pprint(vms)
+
+	# go through the VMs and import them into CS
+	for i, vm in enumerate(vms):
+		print('\nIMPORTING %s\n%s' % (vm['hyperv_vm_name'], '----------'+'-'*len(vm['hyperv_vm_name'])))
+
+		zone = None
+		if 'cs_zone' in vm:
+			zone = vm['cs_zone']
+		elif conf.has_option('CLOUDSTACK', 'default_zone'):
+			zone = conf.get('CLOUDSTACK', 'default_zone')
+
+		domain = None
+		if 'cs_domain' in vm:
+			domain = vm['cs_domain']
+		elif conf.has_option('CLOUDSTACK', 'default_domain'):
+			domain = conf.get('CLOUDSTACK', 'default_domain')
+
+		account = None
+		if 'cs_account' in vm:
+			account = vm['cs_account']
+		elif conf.has_option('CLOUDSTACK', 'default_account'):
+			account = conf.get('CLOUDSTACK', 'default_account')
+
+		network = None
+		if 'cs_network' in vm:
+			network = vm['cs_network']
+		elif conf.has_option('CLOUDSTACK', 'default_network'):
+			network = conf.get('CLOUDSTACK', 'default_network')
+
+		service_offering = None
+		if 'cs_service_offering' in vm:
+			service_offering = vm['cs_service_offering']
+		elif conf.has_option('CLOUDSTACK', 'default_service_offering'):
+			service_offering = conf.get('CLOUDSTACK', 'default_service_offering')
+
+
+		# make sure we have a complete config before we start
+		if zone and domain and account and network and service_offering:
+			# manage the disks
+			if 'disks' in vm and len(vm['disks']) > 0:
+				# register the first disk as a template since it is the root disk
+				print('Creating template for root volume %s...' % (vm['disks'][0]['name']))
+				template = cs.request(dict({
+					'command':'registerTemplate',
+					'name':vm['disks'][0]['name'].replace(' ', '-'),
+					'displaytext':vm['disks'][0]['name'],
+					'format':'VHD',
+					'hypervisor':'XenServer',
+					'ostype':'138', # None
+					'url':vm['disks'][0]['url'],
+					'zoneid':zone,
+					'domainid':domain,
+					'account':account
+				}))
+				if template:
+					print('Template created...')
+
+				# check if there are data disks
+				if len(vm['disks']) > 1:
+					# upload the remaining disks as volumes
+					for disk in vm['disks'][1:]:
+						print('Uploading data volume %s...' % (disk['name']))
+						volume = cs.request(dict({
+							'command':'uploadVolume',
+							'name':disk['name'].replace(' ', '-'),
+							'format':'VHD',
+							'url':disk['url'],
+							'zoneid':zone,
+							'domainid':domain,
+							'account':account
+						}))
+						if volume:
+							print('Volume uploaded...')
+		else:
+			print('We are missing settings fields for %s' % (vm['hyperv_vm_name']))
+
 
 
 	### clean up the running.conf file...
