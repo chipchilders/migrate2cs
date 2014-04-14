dcf9c31cdd469ec08907f1775aab89a6fb68b970
Will Stevens <wstevens@cloudops.com>
2014-04-14 17:32:38 -0400

    more changes

--- migrate_vmware.py
+++ migrate_vmware.py
@@ -6,9 +6,10 @@ from lib.cloudstack import cs
 import json
 import logging
 import os
+import re
 import subprocess
-import time
 import sys
+import time
 if sys.version_info < (2, 7):
 	import lib.subprocess_compat
 	subprocess.check_output = lib.subprocess_compat.check_output
@@ -39,10 +40,12 @@ log.setLevel(logging.INFO)
 conf.set('STATE', 'migrate_error', 'False')
 
 def export_vm(vm_id):
-	# export the vm
 	vms = json.loads(conf.get('STATE', 'vms'))
+	log.info('EXPORTING %s' % (vms[vm_id]['src_name']))
+	vms[vm_id]['clean_name'] = re.sub('[^0-9a-zA-Z]+', '-', vms[vm_id]['src_name'])
 	cmd = ['ovftool'] # command
 	cmd.append('-tt=OVA') # output format
+	cmd.append('-n=%s' % (vms[vm_id]['clean_name'])) # target name
 	cmd.append('vi://%s:%s@%s/%s?ds=%s' % (
 		conf.get('VMWARE', 'username').replace('@', '%40').replace('\\', '%5c').replace('!', '%21'), 
 		conf.get('VMWARE', 'password').replace('@', '%40').replace('\\', '%5c').replace('!', '%21'),
@@ -61,14 +64,14 @@ def export_vm(vm_id):
 				file_path = line.split('File already exists: ')[-1]
 				break
 		if file_path:
-			log.info('Export file exists.  Attempting to remove it so we can export...')
+			log.info('Export file already exists.  Attempting to remove it so we can do a clean export...')
 			rm_error = False
 			try:
 				os.remove(file_path)
 			except:
 				rm_error = True
 				conf.set('STATE', 'migrate_error', 'True')
-				log.error('Failed to remove the existing file...')
+				log.error('Failed to remove the existing export file...')
 				log.error('Could not export %s... \n%s' % (vms[vm_id]['src_name'], initial_error))
 			if not rm_error:
 				log.info('File removed successfully.  Attempting to export again...')
@@ -80,15 +83,24 @@ def export_vm(vm_id):
 		else:
 			log.error('Could not export %s... \n%s' % (vms[vm_id]['src_name'], e.output))
 			conf.set('STATE', 'migrate_error', 'True')
+	if not conf.getboolean('STATE', 'migrate_error'):
+		log.info('Finished exporting %s' % (vms[vm_id]['src_name']))
+		vms[vm_id]['state'] = 'exported'
+		conf.set('STATE', 'migrate', json.dumps(migrate))
+		conf.set('STATE', 'vms', json.dumps(vms))
+		with open('running.conf', 'wb') as f:
+			conf.write(f) # update the file to include the changes we have made
 
 
 def import_vm(vm_id):
 	# import the vm
 	vms = json.loads(conf.get('STATE', 'vms'))
+	log.info('IMPORTING %s' % (vms[vm_id]['src_name']))
 
 def launch_vm(vm_id):
 	# launch the new vm
 	vms = json.loads(conf.get('STATE', 'vms'))
+	log.info('LAUNCHING %s' % (vms[vm_id]['src_name']))
 
 # run the actual migration
 def do_migration():
