92fc33ca3ab1bd94661160ad07666d2d200073fc
Will Stevens <wstevens@cloudops.com>
2014-04-21 13:33:20 -0400

    no changes to path

--- migrate_vmware.py
+++ migrate_vmware.py
@@ -51,7 +51,7 @@ def export_vm(vm_id):
 		conf.get('VMWARE', 'password').replace('@', '%40').replace('\\', '%5c').replace('!', '%21'),
 		conf.get('VMWARE', 'endpoint'),
 		vms[vm_id]['src_dc'],
-		vms[vm_id]['src_path'].replace(' ', ''))) # connection details
+		vms[vm_id]['src_path'])) # connection details
 	cmd.append('/mnt/share/vhds') # destination location
 	log.info('running command:\n%s' % (str(cmd)))
 	output = ''
