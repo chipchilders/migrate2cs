cf908e53082073d35e1d19c987f8dcb6242571d5
Will Stevens <wstevens@cloudops.com>
2014-05-01 17:30:53 -0400

    cleaning cs log on reloads

--- ui_server_vmware.py
+++ ui_server_vmware.py
@@ -152,6 +152,7 @@ def discover_src_vms():
 @bottle.route('/')
 @bottle.view('index')
 def index():
+	open(conf.get('CLOUDSTACK', 'log_file'), 'w').close() # refresh the cs_request.log on reloads
 	variables = {}
 	variables['cs_objs'] = json.dumps(cs_discover_accounts())
 	vms, order = discover_src_vms()
